7cae3215ff7284d4bdf9d25438a164813b178090
unknown <jvt_92@hotmail.com>
2017-11-07 17:08:08 +0100

    Fragments

---
+++ app/src/main/java/com/example/jose/fragment_activity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.jose.fragment_activity;
+
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
